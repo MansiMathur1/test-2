f532d30a382f20658edd2a0bbdd68c5a88bade16
Mansi Mathur <mansi.mathur2005@gmail.com>
2021-06-29 10:22:58 -0400

    This is a testing code.

---
+++ test-2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  test-2
+//
+//  Created by Mansi Mathur on 6/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello world")
+        print("Hello")
+    }
+
+
+}
+
